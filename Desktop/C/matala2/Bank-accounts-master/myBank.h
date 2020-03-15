896de0af160188ca02d02faef56684af72b2acad
Avital-Pikovsky <avitali1996@gmaul.com>
2020-03-15 20:17:19 +0200

    finish

---
+++ Desktop/C/matala2/Bank-accounts-master/myBank.h
@@ -0,0 +1,10 @@
+
+double decimalPlace(double x);
+void open(double a);
+void balance(int account);
+void deposite(int account, double d);
+void withdrawal(int account, double w);
+void closeAccount(int account);
+void interest(double in);
+void printAll();
+void closeAll();
